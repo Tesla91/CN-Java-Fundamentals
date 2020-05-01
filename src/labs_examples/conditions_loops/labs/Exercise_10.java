263b72756e27b01a107630f3570fdb6ad046a587
Nicola Pilcher <nicolapilcher@msn.com>
2020-05-01 13:36:21 +0200

    compleated conditions_loops Exercise 10

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,18 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+        int x = 15;
+
+        while (x > 5){
+            
+            x--;
+            if(x == 13){
+                continue;
+            }
+            System.out.println("x = " + x);
+
+        }
+
+    }
 }
