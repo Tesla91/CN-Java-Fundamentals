f10ab6a32dcf951b1275ede9b50dcf0a0be74487
Nicola Pilcher <nicolapilcher@msn.com>
2020-05-05 19:35:55 +0200

    compleated arrays Exercise 02

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -15,6 +17,15 @@ public class Exercise_02 {
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number between 1 and 10: ");
+        int num = scanner.nextInt();
+
+        if(num > 0 && num <= 10){
+            System.out.println("The number in the " + num + " position of the array is: " + array[num-1]);
+        }else{
+            System.out.println("Sorry your number was not between 1 and 10");
+        }
 
 
     }
