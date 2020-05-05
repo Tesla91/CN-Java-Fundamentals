c20dbee58c35fc6845481100f6c6a38ec221a6f3
Nicola Pilcher <nicolapilcher@msn.com>
2020-05-05 19:27:13 +0200

    compleated arrays Exercise 01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -11,5 +13,58 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int num0 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num3 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num4 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num5 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num6 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num7 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num8 = scanner.nextInt();
+        System.out.print("Please enter another number: ");
+        int num9 = scanner.nextInt();
+
+
+        int[] userNums = new int[10];
+
+        userNums[0] = num0;
+        userNums[1] = num1;
+        userNums[2] = num2;
+        userNums[3] = num3;
+        userNums[4] = num4;
+        userNums[5] = num5;
+        userNums[6] = num6;
+        userNums[7] = num7;
+        userNums[8] = num8;
+        userNums[9] = num9;
+
+        int sum = 0;
+
+        for(int x : userNums){
+            sum += x;
+            System.out.println(sum);
+        }
+        System.out.println("The sum of all your numbers is: " + sum);
+        int average = sum/userNums.length;
+        System.out.println("The average of all your numbers is: " + average );
+
+
+
+
+
+    }
 
 }
