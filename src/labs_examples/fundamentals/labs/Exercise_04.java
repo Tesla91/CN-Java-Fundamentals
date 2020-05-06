d475bd1a037be1005efc9080329fcf19d13c02e3
Nicola Pilcher <nicolapilcher@msn.com>
2020-05-06 13:10:38 +0200

    compleated newly added fundamentals exercise 04

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -4,7 +4,7 @@ package labs_examples.fundamentals.labs;
  * Fundamentals Exercise 4: My Second Program
  *
  *      Write the necessary code to display declare and print each of Java's primitive data types.
- *      Please declare a single variable of each type, give it an appropriate valyue, then print that variable.
+ *      Please declare a single variable of each type, give it an appropriate value, then print that variable.
  *
  */
 
@@ -15,7 +15,23 @@ public class Exercise_04 {
         int i = 1;
         System.out.println("int i is: " + i);
 
+
         // write your code below
+        boolean myBoo = true;
+        byte myByte = 2;
+        short myShort = 23345;
+        long myLong = 983479574345667788l;
+        float myFloat = 938475.76f;
+        double myDouble = 373736455564738392.92;
+        char myChar = 'r';
+
+        System.out.println("boolean myBoo is: " + myBoo);
+        System.out.println("byte myByte is: " + myByte);
+        System.out.println("short myShort is: " + myShort);
+        System.out.println("long myLong is: " + myLong);
+        System.out.println("float myFloat is: " + myFloat);
+        System.out.println("double myDouble is: " + myDouble);
+        System.out.println("char myChar is: " + myChar);
 
     }
 
