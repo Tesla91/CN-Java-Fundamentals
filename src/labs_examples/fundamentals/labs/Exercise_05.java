80c338fb7dcf93e7dddcbd2eef6d278e0df6c679
Nicola Pilcher <nicolapilcher@msn.com>
2020-05-06 13:14:41 +0200

    compleated newly added fundamentals exercise 05

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,29 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int myInt = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
 
+        boolean thisIsTrue = true;
+        if (thisIsTrue && str2.length() == myInt){
+            System.out.println("they are the same");
+        } else{
+            System.out.println("they are not the same");
+        }
+
         // please concatenate str & str2 and set the result to a new String variable below
+        String newString = str + " " + str2;
+        System.out.println(newString);
 
         // please demonstrate the use of any other method that is available to us in the String class
+
         // for example, replace(), substring(), contains(), indexOf() etc
 
+        if(thisIsTrue && str.contains("!")){
+            System.out.println("str contains an !");
+        }
     }
 
 
